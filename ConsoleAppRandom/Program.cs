cad1a4979a07239c0822cbb1cef3fee9a28d9513
Poels Thibault <thibault.poels@immalle.eu>
2017-10-23 09:50:07 +0200

    Add project files.

---
+++ ConsoleAppRandom/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppRandom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
