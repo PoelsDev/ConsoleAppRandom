206b2121054732554b1b58ddd3228abb1e02371b
ThibaultP-immalle <31651240+ThibaultP-immalle@users.noreply.github.com>
2017-10-24 09:11:35 +0200

    Change 1
    Commit 1

--- ConsoleAppRandom/Program.cs
+++ ConsoleAppRandom/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Threading;
 
 namespace ConsoleAppRandom
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const int lowerASCIIBound = 32;
+            const int upperASCIIBound = 128;
+
+            Random rand = new Random();
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.Clear();
+
+            for (int i = 1; i < 10000;)
+            {
+                i += 2;
+                Console.ForegroundColor = (ConsoleColor)rand.Next(10, 16);
+                char c = Convert.ToChar(rand.Next(lowerASCIIBound, upperASCIIBound));             
+                Console.SetCursorPosition(rand.Next(1,115), rand.Next(1,29));
+                Console.Write(c);
+                Console.WriteLine(i);
+                Console.WriteLine();
+                Thread.Sleep(20);
+            }
+
         }
     }
 }
